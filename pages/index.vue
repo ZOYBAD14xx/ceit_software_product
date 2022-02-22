d961ae0c5204a1676ced16e2f6c58b118d1c2829
ZOYBAD14xx <60214442+ZOYBAD14xx@users.noreply.github.com>
2022-02-22 13:46:15 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <center style="margin:20px 0 20px 0">
+    <van-image  justify="center" width="200" height="200" src="https://raw.githubusercontent.com/ZOYBAD14xx/CeitSoftWareWeb/main/image/Logo.png" />
+    <h2 id="text">WhatsApp help</h2>
+    <p id="text">ພຽງແຕ່ທ່ານປ້ອນເບີໂທ ແລະ ຂໍ້ຄວາມ ກໍ່ສາມາດສົ່ງຂໍ້ຄວາມໄດ້</p>
+    </center>
+    <van-form @submit="onSubmit" style="margin-top:30px">
+      <van-cell-group inset>
+        <van-field
+          v-model="phoneNumber"
+          name='phoneNumber'
+          label='ເບີໂທລະສັບ'
+          placeholder='ກະລຸນາໃສ່ເບີໂທ ຕົວຢ່າງ: 20xxxxxxxx'
+        />
+        <van-field
+          v-model="msg"
+          name='msg'
+          label='ຂໍ້ຄວາມ'
+          placeholder='ກະລຸນາໃສ່ຂໍ້ຄວາມທີ່ຢາກສົ່ງ'
+        />
+      </van-cell-group>
+      <div style="margin: 16px">
+        <van-button round block color="#1989fa" native-type="submit">
+          ສົ່ງ
+        </van-button>
+      </div>
+    </van-form>
+    <van-tabbar v-model="active">
+  <van-tabbar-item> &copy; {{ new Date().getFullYear() }} —
+        <strong>CEIT Software</strong>
+  </van-tabbar-item>
+  </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      code: '+856',
+      phoneNumber: '',
+      msg: '',
+      url: 'https://api.whatsapp.com/send'
+    }
+  },
+  methods: {
+    onSubmit () {
+      window.location.href = this.url + '/?phone=' + this.code + this.phoneNumber + '&text=' + this.msg
+    }
+  }
+}
+</script>
+<style>
+#text{
+  color: #169fc6;
+}
+#app{
+  font-family: "BoonHome";
+}
+</style>
+</style>
